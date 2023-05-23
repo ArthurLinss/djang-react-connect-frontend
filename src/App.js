6cfefcb7399141fb915d85784335fc9eeb3c4879
Linß, Arthur <arthur.linss@dlr.de>
2023-05-23 21:07:07 +0200

    frontend update timearrow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { FaUserPlus } from "react-icons/fa";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let color1 = '#ffd700';
+let color2 = '#0057B7';
+
+class App extends React.Component {
+
+    state = { details: [], }
+
+    componentDidMount() 
+    {
+        let data;
+        axios.get("http://localhost:8000")
+        .then(res => {
+            data = res.data;
+            this.setState({
+                details: data
+            });
+        })
+        .catch(err => { })
+    }
+          
+
+    render() 
+    {
+
+        return (
+            <div       
+                style={{
+                    backgroundColor: color1,
+                }}>
+                    
+                <div>
+                <VerticalTimeline>
+                    {this.state.details.map((output, id) => (
+                                <Event output={output} id={id} />
+                        ))}
+                </VerticalTimeline>
+                </div>
+            </div>
+            )
+    }
 }
 
 export default App;
+
+const Event = (props) => {
+    const { output } = props;
+    const { id } = props;
+    return (
+        <VerticalTimelineElement
+                                    className="vertical-timeline-element"
+                                    contentStyle={{ background: color2, color: color1}}
+                                    //contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                                    date= {output.date}  
+                                    layout = {'3-columns'}
+                                    iconStyle={{ background: color2, color: color1 }}
+                                    icon={<FaUserPlus />}
+                                    //position="left"
+                                >
+                                    
+                                    <h3 className="vertical-timeline-element-title">  {output.title}  </h3>
+                                    <h4 className="vertical-timeline-element-subtitle"> {output.subtitle}</h4>
+                                    <p>
+                                    {output.body} 
+                                    </p>
+                                    <iframe 
+                                    src={output.info_link}
+                                    frameborder='0'
+                                    allow='autoplay; encrypted-media'
+                                    allowfullscreen
+                                    title='video'
+                                    />                              
+                                    
+                                </VerticalTimelineElement>
+    );
+  };
---
+++ src/OldApp.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <div>
+      <h1> BB Tech Company </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
